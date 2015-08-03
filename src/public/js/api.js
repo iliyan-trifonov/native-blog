a495cba445a58c94cee12774d9820a594ec9c698
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-03 22:23:45 +0300

    added additional functionality for posts, tags and categories

--- src/public/js/api.js
+++ src/public/js/api.js
@@ -1,8 +1,8 @@
+'use strict';
 //fake Api calls returning promises with data
 export let Api = {
     posts: {
         get: function () {
-            'use strict';
             return Promise.resolve([
                 {
                     title: 'In non urna tristique nisl pellentesque imperdiet',
@@ -83,6 +83,91 @@ export let Api = {
                         'JavaScript',
                         'Web Development'
                     ]
+                }
+            ]);
+        },
+        recent: function () {
+            return Promise.resolve([
+                {
+                    id: 1,
+                    title: 'In non urna tristique nisl pellentesque imperdiet',
+                    slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
+                },
+                {
+                    id: 2,
+                    title: 'In non urna tristique nisl pellentesque imperdiet',
+                    slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
+                },
+                {
+                    id: 3,
+                    title: 'In non urna tristique nisl pellentesque imperdiet',
+                    slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
+                },
+                {
+                    id: 4,
+                    title: 'In non urna tristique nisl pellentesque imperdiet',
+                    slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
+                },
+                {
+                    id: 5,
+                    title: 'In non urna tristique nisl pellentesque imperdiet',
+                    slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
+                }
+            ]);
+        }
+    },
+    tags: {
+        get: function () {
+            return Promise.resolve([
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                },
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                },
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                },
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                },
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                },
+                {
+                    name: 'Tag1',
+                    slug: 'tag1'
+                }
+            ]);
+        }
+    },
+    categories: {
+        get: function () {
+            return Promise.resolve([
+                {
+                    name: 'Test category 1',
+                    slug: 'test-category-1'
+                },
+                {
+                    name: 'Test category 1',
+                    slug: 'test-category-1'
+                },
+                {
+                    name: 'Test category 1',
+                    slug: 'test-category-1'
+                },
+                {
+                    name: 'Test category 1',
+                    slug: 'test-category-1'
+                },
+                {
+                    name: 'Test category 1',
+                    slug: 'test-category-1'
                 }
             ]);
         }
