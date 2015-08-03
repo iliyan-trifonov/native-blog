77ce73bcb6aeaef0db444c0bd4b203c3ecb1b451
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-03 22:24:13 +0300

    added side menu dynamic loading

--- src/public/js/blog.js
+++ src/public/js/blog.js
@@ -8,10 +8,11 @@ export default class Blog {
             '#/about': this.aboutPage.bind(this),
             '#/contact': this.contactPage.bind(this),
             '#/post/([0-9]+)': this.postPage.bind(this)
-        }, win);
+        }, this.sideMenuLoad.bind(this), win);
 
-        //get the main dynamic contents container
+        //get the dynamic contents containers
         this.el = doc.querySelector('.dynamic-content');
+        this.sideMenuEl = doc.querySelector('.dynamic-content-menu');
 
         //parse the current url
         this.router.route(win.location.hash);
@@ -22,6 +23,105 @@ export default class Blog {
             () => this.router.route.call(this.router, win.location.hash),
             false
         );
+    }
+
+    sideMenuLoad () {
+        Promise.all([
+            //TODO: change to: Api.posts.get({recent: true}):
+            Api.posts.recent(),
+            Api.tags.get(),
+            Api.categories.get()
+        ])
+        .then(([posts, tags, cats]) => {
+            let tplBlockStart = `
+            <section class="menu-block">
+                <div class="menu-title">
+                    Recent Posts
+                </div>
+                <ul>`,
+
+            tplBlockEnd = `
+                </ul>
+            </section>`,
+
+            tplItems = [];
+
+            tplItems.push(tplBlockStart);
+
+            for (let post of posts) {
+                tplItems.push(`
+                    <li>
+                        <div class="flex-container">
+                            <div class="flex-item-arrow">&rarr;</div>
+                            <div class="flex-item-title">
+                                <a href="#/post/${post.id}">${post.title}</a>
+                            </div>
+                        </div>
+                    </li>`);
+            }
+
+            tplItems.push(tplBlockEnd);
+
+            this.sideMenuEl.innerHTML = tplItems.join('');
+
+            //
+
+            tplBlockStart = `
+            <section class="menu-block">
+                <div class="menu-title">
+                    Tags
+                </div>
+                <div class="tags">`,
+
+            tplBlockEnd = `
+                </div>
+            </section>`,
+
+            tplItems = [];
+
+            tplItems.push(tplBlockStart);
+
+            for (let tag of tags) {
+                tplItems.push(`<a href="#/tag/${tag.id}">${tag.name}</a> `);
+            }
+
+            tplItems.push(tplBlockEnd);
+
+            this.sideMenuEl.innerHTML += tplItems.join('');
+
+            //
+
+            tplBlockStart = `
+            <section class="menu-block">
+                <div class="menu-title">
+                    Categories
+                </div>
+                <ul>`,
+
+            tplBlockEnd = `
+                </ul>
+            </section>`,
+
+            tplItems = [];
+
+            tplItems.push(tplBlockStart);
+
+            for (let cat of cats) {
+                tplItems.push(`
+                <li>
+                    <div class="flex-container">
+                        <div class="flex-item-arrow">&rarr;</div>
+                        <div class="flex-item-title">
+                            <a href="#/category/${cat.id}">${cat.name}</a>
+                        </div>
+                    </div>
+                </li>`);
+            }
+
+            tplItems.push(tplBlockEnd);
+
+            this.sideMenuEl.innerHTML += tplItems.join('');
+        });
     }
 
     indexPage () {
