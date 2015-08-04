983d80517a75f5e3da566d0278d557b6cabf09f4
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-04 16:57:56 +0300

    replaced static data with logic, the data is taken from the static data store now

--- src/public/js/api.js
+++ src/public/js/api.js
@@ -1,90 +1,54 @@
 'use strict';
+
+import { blogData } from './data.js';
+
+let monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+];
+
+let parseDate = timeStamp => {
+    var date = new Date(timeStamp);
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return `${monthNames[monthIndex]} ${day}, ${year}`;
+};
+
+let populatePosts = posts => {
+    let result = [];
+    for (let id in posts) {
+        if (posts.hasOwnProperty(id)) {
+            let post = posts[id];
+            let p = {
+                id: id,
+                title: post.title,
+                text: post.text,
+                author: {
+                    name: blogData.authors[post.author].name
+                },
+                date: parseDate(post.date),
+                tags: []
+            };
+            for (let tag of post.tags) {
+                p.tags.push(blogData.tags[tag]);
+            }
+            result.push(p);
+        }
+    }
+    return result;
+};
+
 //fake Api calls returning promises with data
 export let Api = {
     posts: {
         get: function () {
-            return Promise.resolve([
-                {
-                    title: 'In non urna tristique nisl pellentesque imperdiet',
-                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
-                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
-                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
-                    ipsum.
-                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
-                    author: {
-                        name: 'Iliyan Trifonov'
-                    },
-                    date: 'July 11, 2015',
-                    tags: [
-                        'JavaScript',
-                        'Web Development'
-                    ]
-                },
-                {
-                    title: 'Morbi efficitur posuere eros',
-                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
-                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
-                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
-                    ipsum.
-                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
-                    author: {
-                        name: 'Iliyan Trifonov'
-                    },
-                    date: 'July 11, 2015',
-                    tags: [
-                        'JavaScript',
-                        'Web Development'
-                    ]
-                },
-                {
-                    title: 'Fusce ante tortor',
-                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
-                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
-                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
-                    ipsum.
-                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
-                    author: {
-                        name: 'Iliyan Trifonov'
-                    },
-                    date: 'July 11, 2015',
-                    tags: [
-                        'JavaScript',
-                        'Web Development'
-                    ]
-                },
-                {
-                    title: 'Maecenas a eros interdum, dictum enim sed, malesuada felis',
-                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
-                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
-                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
-                    ipsum.
-                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
-                    author: {
-                        name: 'Iliyan Trifonov'
-                    },
-                    date: 'July 11, 2015',
-                    tags: [
-                        'JavaScript',
-                        'Web Development'
-                    ]
-                },
-                {
-                    title: 'Donec vel tempor tortor, vitae luctus magna',
-                    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
-                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
-                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
-                    ipsum.
-                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
-                    author: {
-                        name: 'Iliyan Trifonov'
-                    },
-                    date: 'July 11, 2015',
-                    tags: [
-                        'JavaScript',
-                        'Web Development'
-                    ]
-                }
-            ]);
+            return Promise.resolve(
+                populatePosts(blogData.posts)
+            );
         },
         recent: function () {
             return Promise.resolve([
@@ -114,62 +78,51 @@ export let Api = {
                     slug: 'in-non-urna-tristique-nisl-pellentesque-imperdiet'
                 }
             ]);
+        },
+        getById: function (postId) {
+            let result;
+            let post = blogData.posts[postId];
+            if (!post) {
+                result = [];
+            } else {
+                let posts = populatePosts([post]);
+                result = posts.pop();
+            }
+            return Promise.resolve(result);
         }
     },
     tags: {
         get: function () {
-            return Promise.resolve([
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
-                },
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
-                },
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
-                },
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
-                },
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
-                },
-                {
-                    name: 'Tag1',
-                    slug: 'tag1'
+            let tags = blogData.tags;
+            let result = [];
+            for (let id in tags) {
+                if (tags.hasOwnProperty(id)) {
+                    let tag = tags[id];
+                    result.push({
+                        id: id,
+                        name: tag.name,
+                        slug: tag.slug
+                    });
                 }
-            ]);
+            }
+            return Promise.resolve(result);
         }
     },
     categories: {
         get: function () {
-            return Promise.resolve([
-                {
-                    name: 'Test category 1',
-                    slug: 'test-category-1'
-                },
-                {
-                    name: 'Test category 1',
-                    slug: 'test-category-1'
-                },
-                {
-                    name: 'Test category 1',
-                    slug: 'test-category-1'
-                },
-                {
-                    name: 'Test category 1',
-                    slug: 'test-category-1'
-                },
-                {
-                    name: 'Test category 1',
-                    slug: 'test-category-1'
+            let result = [];
+            let cats = blogData.categories;
+            for (let id in cats) {
+                if (cats.hasOwnProperty(id)) {
+                    let cat = cats[id];
+                    result.push({
+                        id: id,
+                        name: cat.name,
+                        slug: cat.slug
+                    });
                 }
-            ]);
+            }
+            return Promise.resolve(result);
         }
     }
 };
