e79ec07d75dedc80fdec9ff66585decfcdea277d
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-04 16:58:49 +0300

    populated with new data and improved for use by the API module

--- src/public/js/data.js
+++ src/public/js/data.js
@@ -1,17 +1,64 @@
 export let blogData = {
-    posts: [
-        {
-            title: '',
-            text: '',
+    posts: {
+        1: {
+            title: 'In non urna tristique nisl pellentesque imperdiet',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
+                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
+                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
+                    ipsum.
+                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
             author: 1,
-            date: '123455678',
+            date: 1436562000000,
+            tags: [1, 2]
+        },
+        2: {
+            title: 'Morbi efficitur posuere eros',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
+                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
+                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
+                    ipsum.
+                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
+            author: 1,
+            date: 1436562000000,
+            tags: [1, 2]
+        },
+        3: {
+            title: 'Fusce ante tortor',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
+                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
+                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
+                    ipsum.
+                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
+            author: 1,
+            date: 1436562000000,
+            tags: [1, 2]
+        },
+        4: {
+            title: 'Maecenas a eros interdum, dictum enim sed, malesuada felis',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
+                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
+                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
+                    ipsum.
+                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
+            author: 1,
+            date: 1436562000000,
+            tags: [1, 2]
+        },
+        5: {
+            title: 'Donec vel tempor tortor, vitae luctus magna',
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at cursus magna.
+                    Nullam congue sapien fringilla aliquet facilisis. Morbi quis elit in arcu suscipit auctor.
+                    Quisque lobortis nulla magna. Curabitur leo libero, elementum tristique augue id, rhoncus iaculis
+                    ipsum.
+                    Phasellus in suscipit lacus, et rhoncus nulla. Donec tristique faucibus efficitur.`,
+            author: 1,
+            date: 1436562000000,
             tags: [1, 2]
         }
-    ],
+    },
     authors: {
         1: {
-            name: 'Iliyan Trifonov',
-            postsNum: 5
+            name: 'Iliyan Trifonov'
         }
     },
     tags: {
