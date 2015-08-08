4648042e289f56c65dd3c0146d0bb47243677f4e
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-08 10:34:43 +0300

    added user module to handle the admin register/log in, uses Parse

---
+++ src/public/js/user.js
@@ -0,0 +1,33 @@
+export default class User {
+    constructor (Parse) {
+        this.Parse = Parse;
+        this.user = this.getUser();
+    }
+
+    getUser () {
+        return this.Parse.User.current();
+    }
+
+    adminLoggedIn () {
+        //this.user = this.getUser();
+        return this.user && true === this.user.get('admin');
+    }
+
+    adminLogin (username, password) {
+        return this.Parse.User.logIn(username, password);
+    }
+
+    adminRegister (username, password) {
+        let user = new this.Parse.User();
+        user.set("username", username);
+        user.set("password", password);
+        user.set("admin", true);
+        return user.signUp();
+    }
+
+    adminExists () {
+        let query = new this.Parse.Query(this.Parse.User);
+        query.equalTo('admin', true);
+        return query.count();
+    }
+}
