09a5e05a06a8a0a347bd8357e7d1d5bf454fa0cc
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-08-08 10:30:12 +0300

    importing config and injecting more objects from the main app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,7 @@
-import Blog from './blog.js';
+import Router from './router';
+import User from './user';
+import { Api } from './api';
+import Blog from './blog';
+import config from './config.json';
 
-new Blog(document, window);
+new Blog(document, window, config, Api, Router, User, Parse);
